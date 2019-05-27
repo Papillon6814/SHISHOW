f44ce97fd3937b964b5b342c6618343e7ee74bc0
Papillon6814 <so1rou.k@gmail.com>
2019-05-27 12:02:29 +0900

    バナー領域

---
+++ shishow/src/components/Banner.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="banner" style="top:top_pixel; left:left_pixel;"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'banner',
+  props: {
+    top_pixel: String,
+    left_pixel: String
+  }
+}
+
+</script>
+
+<style lang="scss">
+  $banner_color: #137480;
+
+  .banner {
+    position: absolute;
+
+    width: 900px;
+    height: 160px;
+
+    background-color: $banner_color;
+  }
+
+</style>
--- shishow/src/views/Home.vue
+++ shishow/src/views/Home.vue
@@ -1,15 +1,36 @@
 <template>
-  <navi></navi>
+  <div id="root">
+    <navi></navi>
+    <banner v-bind='{top_pixel: "120px", left_pixel: "50px;"}'></banner>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import navi from '../components/NavigationBar.vue'
+import banner from '../components/Banner.vue'
 
 export default {
   name: 'home',
   components: {
-    navi
+    navi,
+    banner
   }
 }
 </script>
+
+<style lang="scss">
+
+  $header_color: #ff6f61;
+  $dark_color: #104f58;
+  $footer_color: #23272a;
+  $banner_color: #137480;
+
+  body {
+    padding: 0;
+    margin: 0;
+
+    background-color: $dark_color;
+  }
+
+</style>
