0e028185a3fd5a4aed176508edaf53edb90dd935
Papillon <so1rou.k@gmail.com>
2019-05-28 22:32:22 +0900

    Create Home.vue

---
+++ shishow/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="root">
+    <navi></navi>
+    <banner></banner>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import navi from '../components/NavigationBar.vue'
+import banner from '../components/Banner.vue'
+
+export default {
+  name: 'home',
+  components: {
+    navi,
+    banner
+  }
+}
+</script>
+
+<style lang="scss">
+
+  body {
+    padding: 0;
+    margin: 0;
+
+    background-color: $dark_color;
+  }
+
+</style>
