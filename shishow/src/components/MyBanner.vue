db900d75245bd66293ae5336b8c7c46d7eafda4e
Papillon <so1rou.k@gmail.com>
2019-05-29 01:11:28 +0900

    位置指定

--- shishow/src/components/MyBanner.vue
+++ shishow/src/components/MyBanner.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="banner"></div>
+  <div class="banner"></div>
 </template>
 
 <script>
 
 export default {
-  name: 'banner'
+  name: 'myBanner',
 }
 
 </script>
 
 <style lang="scss">
-  $banner_color: #137480;
 
   .banner {
     position: absolute;
 
-    width: 1000px;
-    height: 200px;
+    top: 120px;
+    left: 50px;
+
+    width: $myBanner_width;
+    height: $myBanner_height;
 
     background-color: $banner_color;
-    border-width: 1px;
-    border-color: black;
   }
 
 </style>
