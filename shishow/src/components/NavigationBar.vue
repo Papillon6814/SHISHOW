74cd05389f8ba95ff5d4a8047144afe00866c0af
Papillon6814 <so1rou.k@gmail.com>
2019-05-29 10:52:52 +0900

    minor

--- shishow/src/components/NavigationBar.vue
+++ shishow/src/components/NavigationBar.vue
@@ -3,14 +3,7 @@
 </template>
 
 <style lang="scss">
-  @import url('../../static/scss/ColorSettings.scss');
-
-  $header_color: #ff6f61;
-  $dark_color: #104f58;
-  $footer_color: #23272a;
-  $banner_color: #137480;
-
-  $header_height: 100px;
+  @import url('../../static/scss/BaseSettings.scss');
 
   #header {
     position: absolute;
