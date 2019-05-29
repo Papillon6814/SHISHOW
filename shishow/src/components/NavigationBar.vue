edbdd484e8e1e2266c861351dadde6d9be3c3131
Papillon <so1rou.k@gmail.com>
2019-05-29 21:07:46 +0900

    adjustment

--- shishow/src/components/NavigationBar.vue
+++ shishow/src/components/NavigationBar.vue
@@ -5,6 +5,11 @@
 <style lang="scss">
   @import url('../../static/scss/BaseSettings.scss');
 
+  $header_color: #ff6f61;
+  $dark_color: #104f58;
+  $footer_color: #23272a;
+  $banner_color: #137480;
+
   #header {
     position: absolute;
 
