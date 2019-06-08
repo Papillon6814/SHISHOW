057d5449bee4e2f59375fde6c1c5129e427e704f
Papillon <so1rou.k@gmail.com>
2019-06-08 22:09:52 +0900

    installed firebase

--- shishow/src/router.js
+++ shishow/src/router.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import dbtest from './views/forDatabase.vue'
+import dbtest2 from './views/forDatabase2.vue'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -12,6 +15,36 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/dbtest',
+      name: 'dbtest',
+      component: dbtest
+    },
+    {
+      path: '/dbtest2',
+      name: 'dbtest2',
+      component: dbtest2
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if(requiresAuth) {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if(user){
+        next()
+      } else {
+        next({
+          path: '/dbtest2',
+          query: { redirect: to.fullPath }
+        })
+      }
+    })
+  } else {
+    next()
+  }
+})
+
+export default router
