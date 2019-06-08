240be5bac01c6d104210fc6b982aba6e33f0fb65
Papillon <so1rou.k@gmail.com>
2019-06-09 02:51:34 +0900

    deleted around compass3

--- shishow/vue.config.js
+++ shishow/vue.config.js
@@ -2,11 +2,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: ['@import "./static/scss/BaseSettings.scss";',
-              '@import "./static/scss/Temporary/sass/TemporaryFile.scss"',
-              '@import "./static/scss/Temporary/stylesheets/TemporaryFile.css"'
-              '@import "./static/JS/TemporaryFile.js"']
-      }
+        data: '@import "./static/scss/BaseSettings.scss";'
     }
   }
 }
