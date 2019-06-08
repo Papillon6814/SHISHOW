ca0b976e1d0c89c0e5cd585c24d26306d5b5a2fc
Emondora1218 <dorachan1216@icloud.com>
2019-06-08 18:34:45 +0900

    achievement

--- shishow/vue.config.js
+++ shishow/vue.config.js
@@ -2,7 +2,10 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: '@import "./static/scss/BaseSettings.scss";'
+        data: ['@import "./static/scss/BaseSettings.scss";',
+              '@import "./static/scss/Temporary/sass/TemporaryFile.scss"',
+              '@import "./static/scss/Temporary/stylesheets/TemporaryFile.css"'
+              '@import "./static/JS/TemporaryFile.js"']
       }
     }
   }
