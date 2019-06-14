eeab40a8185456178960e98916d9fd5e74a3f7ff
Papillon <so1rou.k@gmail.com>
2019-06-15 01:38:51 +0900

    IA

--- shishow/src/components/InputArea.vue
+++ shishow/src/components/InputArea.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="InputBar">
+  <div id="inputBar">
+    <i class="fas fa-smile"></i>
   </div>
 </template>
 
@@ -7,4 +8,19 @@
 </script>
 
 <style lang='scss'>
+  #inputBar {
+    width: 100%;
+    height: 50px;
+
+    background-color: #ddddff;
+
+    .emojiPlace {
+      position: absolute;
+
+      left: 10px;
+      top: 5px;
+
+      font-size: 40px;
+    }
+  }
 </style>
