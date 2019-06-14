970f10128f15e7dd2f85a89bfe510b364baec048
keimoriyama <keischwiiz@gmail.com>
2019-06-14 18:50:26 +0900

    firebaseの設定を追加[

---
+++ shishow/src/views/DirectMsg.vue
@@ -0,0 +1,28 @@
+<template></template>
+
+ <script> 
+import firebase from 'firebase' 
+export default { 
+    name:'Chat'
+     ,
+    props:[ 'SignIn', 'userName', 'userImage' ], 
+    data(){ 
+        return{ 
+            msgList:[], 
+            msg:null, 
+            errorMsg:null 
+            }, 
+        },
+    created(){ 
+        this.loadMsg() 
+    }, 
+    methods:{
+        sengMsg(){
+            if(!this.SignIn || !this.msg)return
+            firebase.database.ref('')
+        }
+    }
+    }
+ </script> 
+ <style>
+</style>
