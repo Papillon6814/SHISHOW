40884620d4017b082b2e85d9513a478b55d7f702
keimoriyama <keischwiiz@gmail.com>
2019-06-14 20:18:42 +0900

    script部分を実装 動かしてない

--- shishow/src/views/DirectMsg.vue
+++ shishow/src/views/DirectMsg.vue
@@ -17,9 +17,31 @@ export default {
         this.loadMsg() 
     }, 
     methods:{
-        sengMsg(){
+        sendMsg(){
             if(!this.SignIn || !this.msg)return
-            firebase.database.ref('')
+            firebase.database.ref('/messages/').push({
+                name:this.userName,
+                text:this.msg,
+                profileImgUrl:this.userImage
+            }).then(data =>{
+                this.errorMsg = null
+                this.msg = null
+            }).catch(error=>{
+                this.errorMsg = '正しく入力してください'
+            });
+        },
+        loadMsg(){
+            firebase.database().ref('/messages/').on('value',(snapshot) =>{
+                if(snapshot){
+                    let rootList = snapshot.val()
+                    let msgList = []
+                    Object.keys(rootList).forEach((val,key)=>{
+                        rootList[val].id = val
+                        msgList.push(rootList[val])
+                    })
+                    this.msgList = msgList
+                }
+            })
         }
     }
     }
