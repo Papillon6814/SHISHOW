07a220b56bbe51c6ff107c17384b41981c827b78
keimoriyama <keischwiiz@gmail.com>
2019-06-15 14:41:31 +0900

    InputAreaとrightAreajsを移植

--- shishow/src/components/dm/InputArea.vue
+++ shishow/src/components/dm/InputArea.vue
@@ -3,7 +3,7 @@
     <div class="smileEmojiPlace">
       <i class="fas fa-smile"></i>
     </div>
-    <input type="text" class="inputText" />
+    <input type="text" class="inputText">
     <div class="checkEmojiPlace">
       <i class="fas fa-check"></i>
     </div>
@@ -11,44 +11,73 @@
 </template>
 
 <script>
-  export default {
-    name: 'inputArea'
+export default {
+  name: "inputArea",
+  props: ["SignIn", "userName", "userImage"],
+  data() {
+    return {
+      msg: null
+    };
+  },
+  methods: {
+    //メッセージを送る
+    sendMsg() {
+      const db = firebase.firestore();
+      if (!this.SignIn || !this.msg) return;
+      //データベースに値をpush
+      db.collection("USER")
+        .add({
+          name: this.userName,
+          text: this.msg,
+          profileImgUrl: this.userImage
+        })
+        //正常な時
+        .then(data => {
+          this.errorMsg = null;
+          this.msg = null;
+        })
+        //エラーの時
+        .catch(error => {
+          this.errorMsg = "殺す";
+        });
+    }
   }
+};
 </script>
 
 <style lang='scss'>
-  #inputBar {
-    width: 100%;
-    height: 100%;;
+#inputBar {
+  width: 100%;
+  height: 100%;
 
-    background-color: #ddddff;
+  background-color: #ddddff;
 
-    .smileEmojiPlace {
-      position: absolute;
+  .smileEmojiPlace {
+    position: absolute;
 
-      left: 3px;
-      top: 5px;
+    left: 3px;
+    top: 5px;
 
-      font-size: 40px;
-    }
-
-    .inputText {
-      position: absolute;
-
-      left: 45px;
-      top: 10px;
-
-      width: 300px;
-      height: 30px;
-    }
-
-    .checkEmojiPlace {
-      position: absolute;
-
-      left: 355px;
-      top: 5px;
-
-      font-size: 40px;
-    }
+    font-size: 40px;
   }
+
+  .inputText {
+    position: absolute;
+
+    left: 45px;
+    top: 10px;
+
+    width: 300px;
+    height: 30px;
+  }
+
+  .checkEmojiPlace {
+    position: absolute;
+
+    left: 355px;
+    top: 5px;
+
+    font-size: 40px;
+  }
+}
 </style>
--- shishow/src/components/dm/rightArea.vue
+++ shishow/src/components/dm/rightArea.vue
@@ -1,9 +1,37 @@
-<template>
-</template>
+<template></template>
 
 <script>
+import firebase from "firebase";
+export default {
+  data() {
+    return {
+      msgList: [],
+      msg: null,
+      errorMsg: null
+    };
+  },
+  created() {
+    this.loadMsg();
+  },
+  methods: {
+    //これまでのメッセージをロード
+    loadMsg() {
+      const db = firebase.firestore();
+      //データベースから値を持ってきてsnapshotに代入
+      db.collection("USER")
+        .get()
+        .then(snapshot => {
+          //snapshotの値はsnapshot.val()で取得できる
+          //let rootList = snapshot.val()
+          let msgList = [];
+          snapshot.forEach(function(doc) {
+            msgList = doc.text;
+          });
+        });
+    }
+  }
+};
 </script>
 
 <style lang='scss'>
-
 </style>
