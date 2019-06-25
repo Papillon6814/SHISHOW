35a273c57401c98ae8c2d07f490c8769fce83805
keimoriyama <keischwiiz@gmail.com>
2019-06-26 00:35:31 +0900

    データベースからユーザー情報の取得してひょうじできたけどなぜか一つしか表示されていない問題

--- shishow/src/components/NormalBanner.vue
+++ shishow/src/components/NormalBanner.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="normalBanner">
-    <span class="iconPicPosition">
-      <div class="iconPic"></div>
-    </span>
-    <div class="achievementPosition1">
-      <div class="achievement"></div>
-    </div>
-    <div class="achievementPosition2">
-      <div class="achievement"></div>
-    </div>
-    <div class="achievementPosition3">
-      <div class="achievement"></div>
-    </div>
-    <div class="usernamePosition">
-      <div class="username">Nakataku</div>
-    </div>
-    <div class="idPosition">
-      <div class="id">qawsedrftgyhujkolp</div>
-    </div>
-    <div class="profilePosition">
-      <div class="profile">
-        新しいことにチャレンジすることが好き!
-        テニス、スキー、スノーボード、ゴルフ、
+    <div v-for="name in userName" v-bind:key="name.id">
+      <span class="iconPicPosition">
+        <div class="iconPic"></div>
+      </span>
+      <div class="achievementPosition1">
+        <div class="achievement"></div>
+      </div>
+      <div class="achievementPosition2">
+        <div class="achievement"></div>
+      </div>
+      <div class="achievementPosition3">
+        <div class="achievement"></div>
+      </div>
+
+      <div class="usernamePosition">
+        <div class="username">{{name.username}}</div>
+      </div>
+      <div class="idPosition">
+        <div class="id">qawsedrftgyhujkolp</div>
+      </div>
+      <div class="profilePosition">
+        <div class="profile">
+          新しいことにチャレンジすることが好き!
+          テニス、スキー、スノーボード、ゴルフ、
+        </div>
       </div>
     </div>
     <button>Panic button</button>
@@ -38,22 +41,24 @@ export default {
   name: "normalBanner",
   data() {
     return {
-      userData: []
+      userData: [],
+      userName: []
     };
   },
   created() {
     this.loadUser();
   },
   methods: {
+    //全てのメルアドを読み込むようにする
     loadUser() {
       const db = firebase.firestore();
-      db.collection("USER").onSnapshot(snapshot => {
-        let userData = [];
-        snapshot.forEach(doc => {
-          userData.push(doc.data());
+      db.collection("USER")
+        .get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            this.userName.push(doc.data());
+          });
         });
-        this.userData = userData;
-      });
     }
   }
 };
--- shishow/src/views/Home.vue
+++ shishow/src/views/Home.vue
@@ -1,15 +1,14 @@
 <template>
-    <div id="root">
-      <navi></navi>
-      <div id="myBannerPosition">
-        <myBanner v-if="userStatus"></myBanner>
-
-      </div>
-      <div class="normalBannerPosition">
-        <div v-for="N in 10" :key="N" v-bind:class="'n'+N">
-          <normalBanner></normalBanner>
-        </div>
-        <!-- <li class="n2">
+  <div id="root">
+    <navi></navi>
+    <div id="myBannerPosition">
+      <myBanner v-if="userStatus"></myBanner>
+    </div>
+    <div class="normalBannerPosition">
+      <!--<div v-for="N in 10" :key="N" v-bind:class="'n'+N">-->
+      <normalBanner></normalBanner>
+      <!--</div>-->
+      <!-- <li class="n2">
           <normalBanner></normalBanner>
         </li>
         <li class="n3">
@@ -17,36 +16,36 @@
         </li>
         <li class="n4">
           <normalBanner></normalBanner>
-        </li> -->
-      </div>
-      <!--
+      </li>-->
+    </div>
+    <!--
       <div class="gameBannerPosition">
         <gameBanner></gameBanner>
-      </div> -->
-    </div>
+    </div>-->
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import navi from '../components/NavigationBar.vue'
-import myBanner from '../components/MyBanner.vue'
-import normalBanner from '../components/NormalBanner.vue'
+import navi from "../components/NavigationBar.vue";
+import myBanner from "../components/MyBanner.vue";
+import normalBanner from "../components/NormalBanner.vue";
 //import gameBanner from '../components/GameBanner.vue'
 
-import firebase from 'firebase'
-import 'firebase/firestore'
-import '@firebase/auth'
-import store from '../store'
+import firebase from "firebase";
+import "firebase/firestore";
+import "@firebase/auth";
+import store from "../store";
 
 export default {
-  name: 'home',
+  name: "home",
   created: function() {
     this.onAuth();
   },
   components: {
     navi,
     myBanner,
-    normalBanner,
+    normalBanner
     //gameBanner
   },
   computed: {
@@ -61,55 +60,56 @@ export default {
     onAuth: function() {
       firebase.auth().onAuthStateChanged(user => {
         user = user ? user : {};
-        store.commit('onAuthStateChanged', user);
-        store.commit('onUserStatusChanged', user.uid ? true : false)
-      })
+        store.commit("onAuthStateChanged", user);
+        store.commit("onUserStatusChanged", user.uid ? true : false);
+      });
     }
   }
-}
-
+};
 </script>
 
 <style lang="scss">
+body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  overflow: hidden;
 
-  body {
-    padding: 0;
-    margin: 0;
-    width: 100%;
-    overflow: hidden;
+  background-color: $dark_color;
+}
 
-    background-color: $dark_color;
-  }
+#myBannerPosition {
+  position: relative;
+  //temporary top
+  top: 60px;
+  left: 10%;
+}
 
-  #myBannerPosition {
-    position: relative;
-    //temporary top
-    top: 60px;
-    left: 10%;
-  }
-
-  /*.wrap{
+/*.wrap{
     overflow: visible;
   }*/
 
-  .normalBannerPosition {
-    $i: 1;
-    @while $i <= 30{
-      .n#{$i}{
-        position: relative;
-        top: 200px + (200px * $i);
-        left: 10%;
-      }
-      $i: $i + 1;
+.normalBannerPosition {
+  $i: 1;
+  @while $i <= 30 {
+    .n#{$i} {
+      position: relative;
+      top: 200px + (200px * $i);
+      left: 10%;
     }
-    list-style: none;
+    $i: $i + 1;
   }
+  list-style: none;
+  margin-left: 10%;
+  width: 100%;
+  position: absolute;
+  padding-top: 200px;
+}
 
-  .gameBannerPosition {
-    position: relative;
-    //temporary top
-    top: 45px;
-    left: 10%;
-  }
-
+.gameBannerPosition {
+  position: relative;
+  //temporary top
+  top: 45px;
+  left: 10%;
+}
 </style>
