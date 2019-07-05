183f188523f2bc640ef0ef261594b73421244b1a
Emondora1218 <dorachan1216@icloud.com>
2019-07-05 20:46:21 +0900

    follow.vue update

--- shishow/src/components/NavigationBar.vue
+++ shishow/src/components/NavigationBar.vue
@@ -22,7 +22,6 @@
         </div>
       </div>
     </div>
-    <div class="logoSentence">SHISHOW</div>
 
     <div id="menuButtons">
       <div class="buttons">
@@ -32,7 +31,9 @@
         <router-link to="/directMessage">
           <i class="fas fa-envelope fa-3x dm"></i>
         </router-link>
-        <i class="fas fa-gamepad fa-3x game"></i>
+        <router-link to="/follow">
+          <i class="fas fa-gamepad fa-3x game"></i>
+        </router-link>
         <router-link to="/notification">
           <i class="fas fa-bell fa-3x bell"></i>
         </router-link>
@@ -54,7 +55,9 @@
             <router-link to="/directMessage">
               <i class="fas fa-envelope fa-3x dm"></i>
             </router-link>
-            <i class="fas fa-gamepad fa-3x game"></i>
+            <router-link to="/follow">
+              <i class="fas fa-gamepad fa-3x game"></i>
+            </router-link>
             <router-link to="/notification">
               <i class="fas fa-bell fa-3x bell"></i>
             </router-link>
@@ -63,9 +66,10 @@
             </router-link>
           </div>
         </div>
+      </div>
     </div>
+    <div class="logoSentence">SHISHOW</div>
   </div>
-</div>
 </template>
 
 <script>
@@ -231,23 +235,6 @@ export default {
     left: 10px;
   }
 
-  .logoSentence {
-    position: absolute;
-
-    font-family: "Pangolin", cursive;
-    top: 20px;
-    font-size: 60px;
-
-    left: 50%;
-    transform: translate(-50%, 0);
-    -webkit-transform: translate(-50%, 0);
-
-    color: $logo_sentence_color;
-
-    text-shadow: 2px 3px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000,
-      -1px 1px 0 #000, 1px 1px 0 #000;
-  }
-
   ::selection {
     background: rgba($secondary-color, 0.3);
     transition: 0.3s ease;
@@ -347,11 +334,31 @@ export default {
   .buttons{
     display: none;
   }
+  .logoSentence {
+    position: absolute;
+
+    font-family: "Pangolin", cursive;
+    top: 20px;
+    font-size: 60px;
+
+    left: 50%;
+    transform: translate(-50%, 0);
+    -webkit-transform: translate(-50%, 0);
+
+    color: $logo_sentence_color;
+
+    text-shadow: 2px 3px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000,
+      -1px 1px 0 #000, 1px 1px 0 #000;
+  }
 }
-@media (max-width: 1360px){　//ナビゲーションバーのレスポンシブ表示
-  /*.logoSentence{
+
+@media (max-width: 1500px){　//ナビゲーションバーのレスポンシブ表示
+  .logoSentence{
+    position: relative;
     top: 100px;
-  }*/
+  }
+}
+@media (max-width: 1100px){
   #nav-drawer {
     position: relative;
   }
@@ -382,34 +389,6 @@ export default {
     display: block;
     content: '';
     cursor: pointer;
-    /*.menu-trigger,
-    .menu-trigger span {
-      display: inline-block;
-      transition: all .4s;
-      box-sizing: border-box;
-    }
-    .menu-trigger {
-      position: relative;
-      width: 50px;
-      height: 44px;
-    }
-    .menu-trigger span {
-      position: absolute;
-      left: 0;
-      width: 100%;
-      height: 4px;
-      background-color: #fff;
-      border-radius: 4px;
-    }
-    .menu-trigger span:nth-of-type(1) {
-      top: 0;
-    }
-    .menu-trigger span:nth-of-type(2) {
-      top: 20px;
-    }
-    .menu-trigger span:nth-of-type(3) {
-      bottom: 0;
-    }*/
   }
   #nav-open span:before {
     top: -22px;//ハンバーガーボタンの一番上の線
@@ -507,20 +486,6 @@ export default {
     box-shadow: 6px 0 25px rgba(0,0,0,.15);
   }
 
-  /*#nav-input:checked ~ #nav-open {
-    .menu-trigger.active span:nth-of-type(1) {
-      -webkit-transform: translateY(20px) rotate(-45deg);
-      transform: translateY(20px) rotate(-45deg);
-    }
-    .menu-trigger.active span:nth-of-type(2) {
-      opacity: 0;
-    }
-    .menu-trigger.active span:nth-of-type(3) {
-      -webkit-transform: translateY(-20px) rotate(45deg);
-      transform: translateY(-20px) rotate(45deg);
-    }
-  }*/
-
   .header-logo-menu{ /*以下コピペにつき，意味が分からない CSSって何なのですか*/
 	  display: flex;
 	  display: -moz-flex;
@@ -533,45 +498,6 @@ export default {
 	  -webkit-flex-direction: row;
 	  -ms-flex-direction: row;
   }
-  /*.menu-trigger,
-  .menu-trigger span {
-    display: inline-block;
-    transition: all .4s;
-    box-sizing: border-box;
-  }
-  .menu-trigger {
-    position: relative;
-    width: 50px;
-    height: 44px;
-  }
-  .menu-trigger span {
-    position: absolute;
-    left: 0;
-    width: 100%;
-    height: 4px;
-    background-color: #fff;
-    border-radius: 4px;
-  }
-  .menu-trigger span:nth-of-type(1) {
-    top: 0;
-  }
-  .menu-trigger span:nth-of-type(2) {
-    top: 20px;
-  }
-  .menu-trigger span:nth-of-type(3) {
-    bottom: 0;
-  }
-  .menu-trigger.active span:nth-of-type(1) {
-    -webkit-transform: translateY(20px) rotate(-45deg);
-    transform: translateY(20px) rotate(-45deg);
-  }
-  .menu-trigger.active span:nth-of-type(2) {
-    opacity: 0;
-  }
-  .menu-trigger.active span:nth-of-type(3) {
-    -webkit-transform: translateY(-20px) rotate(45deg);
-    transform: translateY(-20px) rotate(45deg);
-  }*/
 }
 
 
