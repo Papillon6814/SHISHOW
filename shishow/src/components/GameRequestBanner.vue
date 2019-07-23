f1fd686c2e8e38fee79fa2b6c8061925bd625c07
maru <maru713114@gmail.com>
2019-07-24 02:10:55 +0900

    Update GameRequestBanner.vue

--- shishow/src/components/GameRequestBanner.vue
+++ shishow/src/components/GameRequestBanner.vue
@@ -102,8 +102,8 @@ export default{
     db.collection("GameCollection")
       .doc()
       .set({
-        gamename: this.Gamename,
-        image: image,
+        gamename: Gamename,
+        image:     image,
      })
    },
 
