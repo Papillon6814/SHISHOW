58aab50f3bb4db927ccb2281e1bdac3801579e3f
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-08-02 16:24:15 +0900

    modalとボタン

--- shishow/src/views/SearchResult.vue
+++ shishow/src/views/SearchResult.vue
@@ -1,18 +1,51 @@
 <template>
   <div id="searchRoot">
+    <div id="wrap">
 
-    <navi @search="filterUser"></navi>
-    <div class="searchBannerPosition">
-      <div v-for="N in searchResults.length"
-           :key="N" v-bind:class="'n'+N">
+      <navi @search="filterUser"></navi>
+      <div class="searchBannerPosition">
+        <div v-for="(userinfo, N) in searchResults"
+             :key="N" v-bind:class="'n'+N">
 
-        <normalBanner :user="searchResults[N-1]" 
-        :relations="relation[N-1]" 
-        :signuser="signuser">
-        </normalBanner>
+          <normalBanner
+            :user="searchResults[N]"
+            :relations="relation[N]"
+            :signuser="signuser"
+            @clickNB="NBclick(userinfo)"
+            @clickReqButton="RBclick(userinfo, N)"
+            ref="normal">
+          </normalBanner>
+        </div>
 
+      </div>
     </div>
-  </div>
+
+    <div class="NBModal">
+      <div class="modalPosition">
+        <popupNormalBanner
+          :userInfo="popupUser"
+          @callFade="fadeOut()">
+        </popupNormalBanner>
+      </div>
+    </div>
+
+    <div class="selectModal">
+      <div class="closeBtn" @click="fadeOut()">
+        <i class="fas fa-times"></i>
+      </div>
+      <div class="selectedBannerPosition">
+        <div v-for="N in hisGames.length" :key="N"
+        v-bind:class="'GameLoops'">
+        <div @click="select(hisGames[N-1])">
+          <gameBanner
+            :game="hisGames[N-1]"
+            :signuser="signuser">
+          </gameBanner>
+        </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -23,9 +56,13 @@ import "@firebase/auth";
 
 import navi from "../components/NavigationBar.vue";
 import normalBanner from "../components/NormalBanner";
+import popupNormalBanner from "../components/PopupNormalBanner.vue";
 
 const db = firebase.firestore();
 let currentUser;
+let NBPosition;
+let NBModal;
+let selectModal;
 
 export default {
   name: "search",
@@ -33,6 +70,7 @@ export default {
   components: {
     navi,
     normalBanner,
+    popupNormalBanner
   },
 
   data: function() {
@@ -42,24 +80,11 @@ export default {
       searchResults: [],
       relation:[],
       signuser:"",
+      games: [],
+      hisGames: [],
+      popupUser: '',
+      userId: ''
     };
-  },
-
-  created() {
-    db.collection("USER")
-      .get()
-      .then(doc => {
-        this.users = doc.docs;
-        doc.forEach(docs => {
-          this.filteredUser.push(docs.data());
-        });
-        this.filterUser(/*word = */ this.getSearchWordFromStore);
-      });
-
-    currentUser = firebase.auth().currentUser;
-    this.signuser = {"username":this.$store.getters.user.displayName,
-                     "email":this.$store.getters.user.email}
-
   },
 
   computed: {
@@ -88,7 +113,7 @@ export default {
 
 
             if(docs){
-              
+
               let i;
               for(i=0;i<docs.docs.length && this.users[users_i].data().email != docs.docs[i].id;i++);
               if(i==docs.docs.length){
@@ -107,7 +132,77 @@ export default {
         }
         })
       }
+    },
+
+    NBclick: function(userinfo) {
+      console.log("NBclick");
+      this.showNBModal();
+
+      this.popupUser = userinfo;
+    },
+
+    RBclick: function(userinfo,N) {
+      console.log("RBclick");
+      this.hisGames = [];
+      this.showSelectModal();
+
+      db.collection("USER")
+        .doc(userinfo.email)
+        .collection("GAMES")
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc1 => {
+            this.hisGames.push(doc1);
+          })
+        })
+        this.userId = N;
+    },
+
+    select: function(game) {
+      console.log(game.data().gamename);
+      this.$refs.normal[this.userId].sendFriendReq(game.id);
+      this.fadeOut()
+    },
+
+    showNBModal: function() {
+      NBModal[0].style.display = "block";
+      this.$forceUpdate();
+    },
+
+    showSelectModal: function() {
+      selectModal[0].style.display = "block";
+      this.$forceUpdate();
+    },
+
+    fadeOut: function() {
+      NBModal[0].style.display = "none";
+      selectModal[0].style.display = "none";
+      editModal[0].style.display = "none";
+      this.$forceUpdate();
     }
+  },
+
+  created() {
+    db.collection("USER")
+      .get()
+      .then(doc => {
+        this.users = doc.docs;
+        doc.forEach(docs => {
+          this.filteredUser.push(docs.data());
+        });
+        this.filterUser(/*word = */ this.getSearchWordFromStore);
+      });
+
+    currentUser = firebase.auth().currentUser;
+    this.signuser = {"username":this.$store.getters.user.displayName,
+                     "email":this.$store.getters.user.email}
+
+  },
+
+  mounted: function() {
+
+    NBModal = document.getElementsByClassName("NBModal");
+    selectModal = document.getElementsByClassName("selectModal");
   }
 };
 </script>
@@ -153,5 +248,89 @@ html {
   }
 }
 
+.NBModal {
+  display: none;
 
+  position: absolute;
+
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(0, 0, 0, 0.3);
+
+  z-index: 10000;
+
+  .modalPosition {
+    position: absolute;
+
+    top: 300px;
+    left: 50%;
+
+    width: 65%;
+    height: 100%;
+
+    -webkit-transform: translate(-50%, 0);
+    -moz-transform: translate(-50%, 0);
+    transform: translate(-50%, 0);
+  }
+}
+
+.selectModal {
+  display: none;
+
+  position: absolute;
+
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(0, 0, 0, 0.3);
+
+  z-index: 10000;
+
+  .closeBtn {
+    position: fixed;
+
+    top: 200px;
+    right: 20vw;
+
+    width: 30px;
+    height: 30px;
+
+    font-size: 30px;
+
+    color: $secondary_text;
+    cursor: pointer;
+  }
+
+  .selectedBannerPosition {
+    position: absolute;
+
+    top: 300px;
+    left: 50%;
+
+    transform: translate(-50%, 0);
+    -webkit-transform: translate(-50%, 0);
+    -ms-transform: translate(-50%, 0);
+
+    width: $n_banner_width;
+    height: auto;
+
+    z-index: 10002;
+
+    .GameLoops {
+      position: relative;
+
+      width: 100%;
+      height: $n_banner_height;
+
+      margin-top: 1vw;
+    }
+  }
+}
 </style>
