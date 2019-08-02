66a89fdcfe263c63c9d2178dc50132e9e9364c52
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-08-03 02:26:23 +0900

    Create uploader.vue

---
+++ shishow/src/test/uploader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="root">
+    <input type="file" class="icon">
+    <button @click="click()" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: uploader,
+  methods: {
+    click: function () {
+
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
